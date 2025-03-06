42811406bee9bc1c164be7b073a0db487eda3a1a
Alan Lima <dev.alanlima@gmail.com>
2025-03-06 17:03:01 -0300

    feat: create home page

--- teste-para-front-end/src/app/page.tsx
+++ teste-para-front-end/src/app/page.tsx
@@ -1,101 +1,51 @@
 import Image from "next/image";
+import { Product } from "@/types/Product";
+import Products from "@/components/Organisms/Products";
+import HardwareBadge from "@/components/Atoms/HardwareBadge";
+import ProductCarousel from "@/components/Organisms/ProductCarousel";
 
-export default function Home() {
+export default async function TemplateHome() {
+  const response = await fetch("http://localhost:3000/api/products");
+  const products: Product[] = await response.json();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main>
+      <div className="flex flex-col gap-10 xs:w-fit xl:w-full  ">
+        <section className="bg-[#010101] w-fit xl:w-full">
+          <div className="p-4 max-w-[1440px] flex flex-col xs:gap-8 gap-4 xs:p-10 mx-auto">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="w-full rounded-sm hidden xs:block"
+              alt="Banner"
+              src="/Banner-lg.png"
+              width={1360}
+              height={366}
+              sizes="100vw"
+              priority
+              placeholder="empty"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <Image
+              className="w-full rounded-sm xs:hidden"
+              alt="Banner"
+              src="/Banner.png"
+              width={1360}
+              height={366}
+              sizes="100vw"
+              priority
+              placeholder="empty"
+            />
+            <Products products={products} />
+          </div>
+        </section>
+        <section className="xs:grid xs:place-items-center w-full">
+          <div className="flex xs:max-w-[1440px] flex-col gap-4 px-4">
+            <h2 className="font-medium text-xl xs:text-2xl flex gap-2 items-center">
+              Placas de vídeo
+              <HardwareBadge />
+            </h2>
+            <ProductCarousel products={products} />
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
