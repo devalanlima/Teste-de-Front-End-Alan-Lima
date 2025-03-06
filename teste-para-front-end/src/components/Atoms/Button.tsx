f796bb69b727ffb8f6312b653abde2ec9a185280
Alan Lima <dev.alanlima@gmail.com>
2025-03-06 16:57:56 -0300

    feat: add new outline style

--- teste-para-front-end/src/components/Atoms/Button.tsx
+++ teste-para-front-end/src/components/Atoms/Button.tsx
@@ -1,30 +1,38 @@
 import { Variant } from "@/types/Variant";
-import { twMerge } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
   variant?: Variant;
+  outline?: boolean;
 }
 
 export default function Button({
   children,
   variant = "default",
   className,
+  outline,
   ...rest
 }: Props) {
   const buttonVariants: Record<Variant, string> = {
-    default: "bg-accent text-white hover:bg-[#3722BA]",
-    logitech: "bg-logitech-accent hover:bg-[#0085AC] text-text text-xl",
+    default: twJoin(
+      "bg-accent text-white hover:bg-[#3722BA]",
+      outline && " bg-transparent text-accent outline outline-accent"
+    ),
+    logitech: twJoin(
+      "bg-logitech-accent hover:bg-[#0085AC] text-xl text-text",
+      outline &&
+        " bg-transparent text-logitech-accent outline outline-logitech-accent"
+    ),
   };
 
+  const buttonStyle = twJoin(
+    buttonVariants[variant],
+    "p-2 font-dm-sans font-bold cursor-pointer"
+  );
+
   return (
-    <button
-      className={twMerge(
-        `${buttonVariants[variant]} p-2 font-dm-sans font-bold cursor-pointer`,
-        className
-      )}
-      {...rest}
-    >
+    <button className={twMerge(buttonStyle, className)} {...rest}>
       {children}
     </button>
   );
