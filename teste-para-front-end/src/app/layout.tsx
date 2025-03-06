52ddeff9970a56ad1ab2eac3bc47cafe6d19bf2b
Alan Lima <dev.alanlima@gmail.com>
2025-03-06 12:28:43 -0300

    feat: add new DM-Sans font and background color to body

--- teste-para-front-end/src/app/layout.tsx
+++ teste-para-front-end/src/app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const DMSans = DM_Sans({
   subsets: ["latin"],
 });
 
@@ -23,12 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-BR">
+      <body className={`${DMSans.className} bg-background`}>{children}</body>
     </html>
   );
 }
