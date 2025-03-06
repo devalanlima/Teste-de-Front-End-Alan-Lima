54aab3c2f1cd853ee32eb2ac4483712d71061c9d
Alan Lima <dev.alanlima@gmail.com>
2025-03-06 12:31:24 -0300

    feat: create logitech badge

---
+++ teste-para-front-end/src/components/Atoms/LogitechBadge.tsx
@@ -0,0 +1,10 @@
+import IconLogitech from "./icons/IconLogitech";
+
+export default function LogitechBadge() {
+  return (
+    <div className="flex gap-1 text-sm xs:text-lg items-center w-fit justify-center text-logitech-accent">
+      <IconLogitech />
+      LOGITECH
+    </div>
+  );
+}
